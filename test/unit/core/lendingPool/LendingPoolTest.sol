2f684c8c5fa33a774dd55a55c8797d9158852b3c
Nikos Chondromatidis <nchondromatidis@gmail.com>
2024-02-05 13:14:33 +0200

    added depositFirstLossCapital in test_borrowLoan arrange

--- test/unit/core/lendingPool/LendingPoolTest.sol
+++ test/unit/core/lendingPool/LendingPoolTest.sol
@@ -285,6 +285,30 @@ contract LendingPoolTest is LendingPoolTestUtils {
         assertEq(pendingPool.ownerOf(wNftId_alice), address(0));
     }
 
+    function test_depositFirstLossCapital() public {
+        // ### ARRANGE ###
+        LendingPoolDeployment memory lpd = _createDefaultLendingPool();
+
+        uint256 requestDepositAmount_alice = 100 * 10 ** 6;
+        uint256 dNftId_alice = _requestDeposit(alice, lpd.lendingPool, lpd.tranches[0], requestDepositAmount_alice);
+
+        uint256 requestDepositAmount_bob = 250 * 10 ** 6;
+        uint256 dNftId_bob = _requestDeposit(bob, lpd.lendingPool, lpd.tranches[1], requestDepositAmount_bob);
+
+        uint256 acceptDepositAmount_alice = 40 * 10 ** 6;
+        _acceptDepositRequest(lpd.lendingPool, dNftId_alice, acceptDepositAmount_alice);
+
+        uint256 acceptedDepositAmount_bob = 250 * 10 ** 6;
+        _acceptDepositRequest(lpd.lendingPool, dNftId_bob, acceptedDepositAmount_bob);
+
+        // ### ACT ###
+        _depositFirstLossCapital(lendingPoolLoanAdmin, lpd.lendingPool, 50 * 10 ** 6);
+        _depositFirstLossCapital(lendingPoolLoanAdmin, lpd.lendingPool, 10 * 10 ** 6);
+
+        // ### ASSERT ###
+        assertEq(mockUsdc.balanceOf(lpd.lendingPool) - ILendingPool(lpd.lendingPool).totalSupply(), 60 * 10 ** 6);
+    }
+
     function test_borrowLoan() public {
         // ### ARRANGE ###
         LendingPoolDeployment memory lpd = _createDefaultLendingPool();
@@ -301,6 +325,8 @@ contract LendingPoolTest is LendingPoolTestUtils {
         uint256 acceptedDepositAmount_bob = 250 * 10 ** 6;
         _acceptDepositRequest(lpd.lendingPool, dNftId_bob, acceptedDepositAmount_bob);
 
+        _depositFirstLossCapital(lendingPoolLoanAdmin, lpd.lendingPool, 50 * 10 ** 6);
+
         uint256 lendingPoolTokenTotalSupplyBefore = ILendingPool(lpd.lendingPool).totalSupply();
 
         // ### ACT ###
@@ -313,7 +339,7 @@ contract LendingPoolTest is LendingPoolTestUtils {
         _borrowLoan(lendingPoolLoanAdmin, lpd.lendingPool, 200 * 10 ** 6);
 
         // ### ASSERT ###
-        assertEq(mockUsdc.balanceOf(lpd.lendingPool), 90 * 10 ** 6);
+        assertEq(mockUsdc.balanceOf(lpd.lendingPool), 140 * 10 ** 6);
         assertEq(mockUsdc.balanceOf(lendingPoolLoanAdmin), 200 * 10 ** 6);
         assertEq(ILendingPool(lpd.lendingPool).totalSupply(), lendingPoolTokenTotalSupplyBefore);
     }
@@ -344,28 +370,5 @@ contract LendingPoolTest is LendingPoolTestUtils {
         assertEq(mockUsdc.balanceOf(lpd.lendingPool), 190 * 10 ** 6);
         assertEq(mockUsdc.balanceOf(lendingPoolLoanAdmin), 0);
         assertEq(ILendingPool(lpd.lendingPool).totalSupply(), lendingPoolTokenTotalSupplyBefore);
-    }
-
-    function test_depositFirstLossCapital() public {
-        // ### ARRANGE ###
-        LendingPoolDeployment memory lpd = _createDefaultLendingPool();
-
-        uint256 requestDepositAmount_alice = 100 * 10 ** 6;
-        uint256 dNftId_alice = _requestDeposit(alice, lpd.lendingPool, lpd.tranches[0], requestDepositAmount_alice);
-
-        uint256 requestDepositAmount_bob = 250 * 10 ** 6;
-        uint256 dNftId_bob = _requestDeposit(bob, lpd.lendingPool, lpd.tranches[1], requestDepositAmount_bob);
-
-        uint256 acceptDepositAmount_alice = 40 * 10 ** 6;
-        _acceptDepositRequest(lpd.lendingPool, dNftId_alice, acceptDepositAmount_alice);
-
-        uint256 acceptedDepositAmount_bob = 250 * 10 ** 6;
-        _acceptDepositRequest(lpd.lendingPool, dNftId_bob, acceptedDepositAmount_bob);
-
-        // ### ACT ###
-        _depositFirstLossCapital(lendingPoolLoanAdmin, lpd.lendingPool, 50 * 10 ** 6);
-
-        // ### ASSERT ###
-        assertEq(mockUsdc.balanceOf(lpd.lendingPool) - ILendingPool(lpd.lendingPool).totalSupply(), 50 * 10 ** 6);
     }
 }
