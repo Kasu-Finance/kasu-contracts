4d89dd9591313ad85a89011d712e0e360dd4c781
Nikos Chondromatidis <nchondromatidis@gmail.com>
2024-03-26 01:15:15 +0200

    added function descriptions for IClearingManager

--- src/core/interfaces/clearing/IClearingManager.sol
+++ src/core/interfaces/clearing/IClearingManager.sol
@@ -11,9 +11,29 @@ enum TaskStatus {
 }
 
 interface IClearingManager {
+    /**
+     * @notice Should be called if you need to overwrite the default clearing configuration.
+     * @dev
+     * Should be optionally called before doClearing. Called once.
+     * @param lendingPool The lending pool that clearing config will be registered.
+     * @param epoch The epoch to run clearing against.
+     * @param clearingConfig The clearing config that will overwrite the default one.
+     */
     function registerClearingConfig(address lendingPool, uint256 epoch, ClearingConfiguration calldata clearingConfig)
         external;
 
+    /**
+     * @notice Runs all the tasks required for clearing to succeed. Tasks run in sequence. Some tasks can be
+     * accomplished in multiple transactions.
+     * @dev
+     * This task can be completed in multiple transactions.
+     * @param lendingPool The lending pool that clearing config will be registered.
+     * @param targetEpoch The epoch to run clearing against.
+     * @param pendingRequestsPriorityCalculationBatchSize The amount of user requests that `pending requests priority
+     * calculation` will process in one transaction.
+     * @param acceptedRequestsExecutionBatchSize The amount of user requests that `accepted requests execution` task
+     * will process in one transaction.
+     */
     function doClearing(
         address lendingPoolAddress,
         uint256 targetEpoch,
@@ -21,6 +41,12 @@ interface IClearingManager {
         uint256 acceptedRequestsExecutionBatchSize
     ) external;
 
+    /**
+     * @notice Returns the active config for the clearing task.
+     * @param lendingPool The lending pool of the clearing config.
+     * @param epoch The epoch of the clearing config.
+     * @param clearingConfig The clearing config that will overwrite the default one.
+     */
     function getClearingConfig(address lendingPoolAddress, uint256 epoch)
         external
         view
