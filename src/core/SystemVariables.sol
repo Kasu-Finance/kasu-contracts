d907ad19e00728e8d7bdfc35730fc5c6600ed7a2
ryptoc <dannoc1996@gmail.com>
2024-04-05 15:21:58 +0200

    Added SystemVariables comments

--- src/core/SystemVariables.sol
+++ src/core/SystemVariables.sol
@@ -14,6 +14,10 @@ import "../shared/AddressLib.sol";
  * @custom:member firstEpochStartTimestamp The timestamp of the start of the first epoch. Should be in the past.
  * @custom:member clearingPeriodLength The length of the clearing period.
  * @custom:member performanceFee The performance fee.
+ * @custom:member loyaltyThresholds The loyalty level threshold percentages.
+ * @custom:member defaultTrancheInterestChangeEpochDelay Default epoch delay when tranche interest rate is changed.
+ * @custom:member ecosystemFeeRate The ecosystem fee percentage rate.
+ * @custom:member protocolFeeRate The protocol fee percentage rate.
  */
 struct SystemVariablesSetup {
     uint256 firstEpochStartTimestamp;
@@ -27,39 +31,70 @@ struct SystemVariablesSetup {
 
 /**
  * @notice Kasu system variables contract.
- * @dev This contract is used to store and manage Kasu system variables.
- * It stores epoch, KSU epoch price and platform fee.
+ * @dev This contract is used to store and manage global Kasu system variables.
+ * It manages epoch, KSU epoch price and platform fee and other global variables.
  * Kasu epoch number always starts from 0.
  */
 contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializable {
+    /// @notice The KSU token price contract.
     IKsuPrice public immutable ksuPrice;
 
+    /// @notice The timestamp of the start of the first epoch.
     uint256 private constant _epochDuration = 1 weeks;
+
+    /// @notice The timestamp of the start of the first epoch.
     uint256 private _firstEpochStartTimestamp;
+
+    /// @notice The length of the clearing period.
     uint256 private _clearingPeriodLength;
 
+    /// @notice The epoch number when the price was last updated.
     uint256 private _priceUpdateEpoch;
+
+    /// @notice The price of the KSU token for the epoch.
     uint256 private _ksuTokenPrice;
 
+    /// @notice The performance fee percentage.
     uint256 private _performanceFee;
 
+    /// @notice The loyalty level threshold percentages.
     uint256[] private _loyaltyThresholds;
 
+    /// @notice Flag to enable/disable users to deposits to junior tranche only if the user has rKSU.
     bool private _userCanOnlyDepositToJuniorTrancheWhenHeHasRKSU;
 
+    /// @notice Default epoch delay when tranche interest rate is changed.
     uint256 private _defaultTrancheInterestChangeEpochDelay;
 
+    /// @notice The maximum allowed interest rate per tranche.
+    /// @dev This is to prevent Pool Manager from mistakenly setting a huge epoch interest rate.
     uint256 private _maxTrancheInterestRate;
 
+    /// @notice The minimum tranche count per lending pool.
     uint256 private _minTrancheCountPerLendingPool;
+
+    /// @notice The maximum tranche count per lending pool.
     uint256 private _maxTrancheCountPerLendingPool;
 
+    /// @notice The ecosystem fee percentage rate.
+    /// @dev Denominated in FULL_PERCENT.
     uint256 private _ecosystemFeeRate;
+
+    /// @notice The protocol fee percentage rate.
+    /// @dev Denominated in FULL_PERCENT.
     uint256 private _protocolFeeRate;
+
+    /// @notice The protocol fee receiver.
     address private _protocolFeeReceiver;
 
-    TrancheInfo[] public _trancheInfo;
+    /// @notice The tranche name information.
+    TrancheInfo[] public _trancheNameInfo;
 
+    /**
+     * @notice Constructor.
+     * @param ksuPrice_ The KSU token price contract.
+     * @param controller_ The Kasu controller contract.
+     */
     constructor(IKsuPrice ksuPrice_, IKasuController controller_) KasuAccessControllable(controller_) {
         AddressLib.checkIfZero(address(ksuPrice_));
         AddressLib.checkIfZero(address(controller_));
@@ -68,6 +103,10 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
         _disableInitializers();
     }
 
+    /**
+     * @notice Initializes the contract.
+     * @param systemVariablesSetup The system variables setup structure.
+     */
     function initialize(SystemVariablesSetup memory systemVariablesSetup) external initializer {
         if (
             systemVariablesSetup.firstEpochStartTimestamp < block.timestamp
@@ -96,12 +135,11 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
         _minTrancheCountPerLendingPool = 1;
         _maxTrancheCountPerLendingPool = 3;
 
-        _trancheInfo.push(TrancheInfo("Junior Tranche", "JR"));
-        _trancheInfo.push(TrancheInfo("Mezzanine Tranche", "MZ"));
-        _trancheInfo.push(TrancheInfo("Senior Tranche", "SR"));
+        _trancheNameInfo.push(TrancheInfo("Junior Tranche", "JR"));
+        _trancheNameInfo.push(TrancheInfo("Mezzanine Tranche", "MZ"));
+        _trancheNameInfo.push(TrancheInfo("Senior Tranche", "SR"));
 
-        _ecosystemFeeRate = systemVariablesSetup.ecosystemFeeRate;
-        _protocolFeeRate = systemVariablesSetup.protocolFeeRate;
+        _setFeeRates(systemVariablesSetup.ecosystemFeeRate, systemVariablesSetup.protocolFeeRate);
     }
 
     // EPOCH
@@ -207,7 +245,6 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
 
     function _updateKsuTokenPrice() internal {
         _priceUpdateEpoch = getCurrentEpochNumber();
-
         _ksuTokenPrice = ksuPrice.getKsuTokenPrice();
 
         emit KsuTokenPriceUpdated(_priceUpdateEpoch, _ksuTokenPrice);
@@ -216,7 +253,8 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
     // performance fee
 
     /**
-     * @dev Sets the performance fee.
+     * @notice Sets the performance fee.
+     * @dev The performance fee is denominated in FULL_PERCENT and cannot exceed it.
      * @param performanceFee_ The new performance fee.
      */
     function setPerformanceFee(uint256 performanceFee_) external whenNotPaused onlyAdmin {
@@ -245,6 +283,10 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
 
     /**
      * @notice Returns the loyalty thresholds.
+     * @dev
+     * The loyalty thresholds are denominated in FULL_PERCENT.
+     * Index 0 represents loyalty level 1, and the last index is the highest loyalty level.
+     * Loyalty level 0 is assumed by default and it should have 0% threshold.
      * @return The loyalty thresholds.
      */
     function loyaltyThresholds() external view returns (uint256[] memory) {
@@ -252,7 +294,11 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
     }
 
     /**
-     * @notice Sets the loyalty thresholds.
+     * @notice Sets the loyalty thresholds percentages.
+     * @dev
+     * The loyalty thresholds are denominated in FULL_PERCENT and values must be in ascending order.
+     * Index 0 represents loyalty level 1, and the last index is the highest loyalty level.
+     * If array has no elements then there is only one loyalty level.
      * @param loyaltyThresholds_ The new loyalty thresholds array.
      */
     function setLoyaltyThresholds(uint256[] memory loyaltyThresholds_) external whenNotPaused onlyAdmin {
@@ -277,38 +323,42 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
         }
 
         _loyaltyThresholds = loyaltyThresholds_;
+
+        // TODO: emit event
     }
 
     // LENDING POOL
 
     /**
      * @notice Returns whether users can only deposit to junior tranches only when having rKSU.
-     * @return true if they are only allowed to deposit to junior tranche when they have rKSU, false the other way around
+     * @return true if they are only allowed to deposit to junior tranche when they have rKSU, false the other way around.
      */
     function getUserCanOnlyDepositToJuniorTrancheWhenHeHasRKSU() external view returns (bool) {
         return _userCanOnlyDepositToJuniorTrancheWhenHeHasRKSU;
     }
 
     /**
-     * @notice Sets whether users are allowed to deposit only when the own rKSU
-     * @param value Set to true if they are only allowed to deposit to junior tranche when they have rKSU, false the other way around
+     * @notice Sets whether users are allowed to deposit to junior tranche only when the own rKSU.
+     * @param value Set to true if they are only allowed to deposit to junior tranche when they have rKSU, false the other way around.
      */
     function setUserCanOnlyDepositToJuniorTrancheWhenHeHasRKSU(bool value) external whenNotPaused onlyAdmin {
         _userCanOnlyDepositToJuniorTrancheWhenHeHasRKSU = value;
+
+        // TODO: emit event
     }
 
     // TRANCHE
 
     /**
-     * @notice Returns default epoch delay when tranche interest rate is changed
-     * @return The default epoch delay when tranche interest rate is changed
+     * @notice Returns default epoch delay when tranche interest rate is changed.
+     * @return The default epoch delay when tranche interest rate is changed.
      */
     function defaultTrancheInterestChangeEpochDelay() external view returns (uint256) {
         return _defaultTrancheInterestChangeEpochDelay;
     }
 
     /**
-     * @notice Sets default epoch delay when tranche interest rate is changed
+     * @notice Sets default epoch delay when tranche interest rate is changed.
      * @param defaultTrancheInterestChangeEpochDelay_ The new default epoch delay.
      */
     function setDefaultTrancheInterestChangeEpochDelay(uint256 defaultTrancheInterestChangeEpochDelay_)
@@ -317,86 +367,100 @@ contract SystemVariables is ISystemVariables, KasuAccessControllable, Initializa
         onlyAdmin
     {
         _defaultTrancheInterestChangeEpochDelay = defaultTrancheInterestChangeEpochDelay_;
+
+        // TODO: emit event
     }
 
     /**
-     * @notice Returns the maximum allowed interest rate allowed in tranche
-     * @return The maximum interest rate allowed in tranche
+     * @notice Returns the maximum allowed interest rate allowed in tranche.
+     * @return The maximum interest rate allowed in tranche.
      */
     function maxTrancheInterestRate() external view returns (uint256) {
         return _maxTrancheInterestRate;
     }
 
     /**
-     * @notice Sets the maximum allowed interest rate per tranche
-     * @param maxTrancheInterestRate_ maximum allowed interest rate per tranche
+     * @notice Sets the maximum allowed interest rate per tranche.
+     * @param maxTrancheInterestRate_ maximum allowed interest rate per tranche.
      */
     function setMaxTrancheInterestRate(uint256 maxTrancheInterestRate_) public whenNotPaused onlyAdmin {
         _maxTrancheInterestRate = maxTrancheInterestRate_;
+
+        // TODO: emit event
     }
 
     /**
-     * @notice Returns the minimum tranche count per lending pool
-     * @return The minimum tranche count per lending pool
+     * @notice Returns the minimum tranche count per lending pool.
+     * @return The minimum tranche count per lending pool.
      */
     function minTrancheCountPerLendingPool() external view returns (uint256) {
         return _minTrancheCountPerLendingPool;
     }
 
     /**
-     * @notice Returns the maximum tranche count per lending pool
-     * @return The maximum tranche count per lending pool
+     * @notice Returns the maximum tranche count per lending pool.
+     * @return The maximum tranche count per lending pool.
      */
     function maxTrancheCountPerLendingPool() external view returns (uint256) {
         return _maxTrancheCountPerLendingPool;
     }
 
     /**
-     * @notice Returns the default names and symbols for tranche
-     * @param index The index of the tranche
-     * @return The default name and symbol for tranche
+     * @notice Returns the default names and symbols for tranche.
+     * @param index The index of the tranche.
+     * @return The default name and symbol for tranche.
      */
     function getTrancheInfo(uint256 index) external view returns (TrancheInfo memory) {
-        return _trancheInfo[index];
+        return _trancheNameInfo[index];
     }
 
     // FEES
 
     /**
-     * @notice Returns the protocol fee rate
-     * @return ecosystemFeeRate The ecosystem fee rate
-     * @return protocolFeeRate The protocol fee rate
+     * @notice Returns the protocol fee rate.
+     * @return ecosystemFeeRate The ecosystem fee rate.
+     * @return protocolFeeRate The protocol fee rate.
      */
     function getFeeRates() external view returns (uint256 ecosystemFeeRate, uint256 protocolFeeRate) {
         return (_ecosystemFeeRate, _protocolFeeRate);
     }
 
     /**
-     * @notice Sets the split ratio for the fees.
-     * @param ecosystemFeeRate The ecosystem fee rate
+     * @notice Sets the split ratio for the collected fees.
+     * @dev The sum of the ecosystem and protocol fee rates must be equal to FULL_PERCENT.
+     * @param ecosystemFeeRate The ecosystem fee rate.
+     * @param protocolFeeRate The protocol fee rate.
      */
     function setFeeRates(uint256 ecosystemFeeRate, uint256 protocolFeeRate) external whenNotPaused onlyAdmin {
+        _setFeeRates(ecosystemFeeRate, protocolFeeRate);
+    }
+
+    function _setFeeRates(uint256 ecosystemFeeRate, uint256 protocolFeeRate) private {
         if (ecosystemFeeRate + protocolFeeRate != FULL_PERCENT) {
             revert InvalidConfiguration();
         }
         _ecosystemFeeRate = ecosystemFeeRate;
         _protocolFeeRate = protocolFeeRate;
+
+        // TODO: emit event
     }
 
     /**
-     * @notice Returns the protocol fee receiver
-     * @return The protocol fee receiver
+     * @notice Returns the protocol fee receiver.
+     * @return The protocol fee receiver.
      */
     function getProtocolFeeReceiver() public view returns (address) {
         return _protocolFeeReceiver;
     }
 
     /**
-     * @notice Sets the protocol fee receiver
-     * @param receiver The protocol fee receiver
+     * @notice Sets the protocol fee receiver.
+     * @param receiver The protocol fee receiver.
      */
     function setProtocolFeeReceiver(address receiver) public whenNotPaused onlyAdmin {
         AddressLib.checkIfZero(receiver);
         _protocolFeeReceiver = receiver;
+
+        // TODO: emit event
     }
 }
