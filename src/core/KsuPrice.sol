14893704b7a52f6e64651a1d6d94d68592922926
ryptoc <dannoc1996@gmail.com>
2024-04-05 04:04:24 +0200

    Added KsuPrice comments

--- src/core/KsuPrice.sol
+++ src/core/KsuPrice.sol
@@ -5,10 +5,21 @@ import "./interfaces/IKsuPrice.sol";
 import "../../vendor/chainsight/IOracle.sol";
 import "../shared/AddressLib.sol";
 
+/**
+ * @title KSU token price contract
+ * @notice This contract is used to get the current price of the KSU token using Chainsight oracle.
+ */
 contract KsuPrice is IKsuPrice {
+    /// @notice Chainsight oracle contract.
     IOracle public immutable oracle;
+    /// @notice Address of the oracle sender.
     address public immutable oracleSender;
 
+    /**
+     * @notice Constructor.
+     * @param oracle_ Chainsight oracle contract.
+     * @param oracleSender_ Address of the oracle sender.
+     */
     constructor(IOracle oracle_, address oracleSender_) {
         AddressLib.checkIfZero(address(oracle_));
         AddressLib.checkIfZero(oracleSender_);
@@ -17,6 +28,10 @@ contract KsuPrice is IKsuPrice {
         oracleSender = oracleSender_;
     }
 
+    /**
+     * @notice Get the current price of the KSU token.
+     * @return Price of the KSU token.
+     */
     function getKsuTokenPrice() external view returns (uint256) {
         return oracle.readAsUint256(oracleSender);
     }
