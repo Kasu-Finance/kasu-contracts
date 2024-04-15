42d8e4e1868987ef57dc4e397adc875e561adb48
ryptoc <dannoc1996@gmail.com>
2024-04-15 03:51:29 +0200

    Updated KasuController comments

--- src/shared/access/KasuController.sol
+++ src/shared/access/KasuController.sol
@@ -8,7 +8,7 @@ import "./Roles.sol";
 import "../AddressLib.sol";
 
 /**
- * @notice Kasu access control management
+ * @notice Kasu access control management.
  */
 contract KasuController is AccessControlUpgradeable, PausableUpgradeable, IKasuController {
     /* ========== CONSTRUCTOR ========== */
@@ -19,6 +19,11 @@ contract KasuController is AccessControlUpgradeable, PausableUpgradeable, IKasuC
 
     /* ========== INITIALIZER ========== */
 
+    /**
+     * @notice Initializes the Kasu controller contract.
+     * @param admin Address of the initial Kasu admin.
+     * @param factory Address of the Kasu lending pool factory.
+     */
     function initialize(address admin, address factory) public initializer {
         AddressLib.checkIfZero(admin);
         AddressLib.checkIfZero(factory);
@@ -29,16 +34,32 @@ contract KasuController is AccessControlUpgradeable, PausableUpgradeable, IKasuC
 
     /* ========== EXTERNAL VIEW FUNCTIONS ========== */
 
+    /**
+     * @notice Returns true if an account has a role for a lending pool.
+     * @param lendingPool Address of the lending pool.
+     * @param role Role id to look for.
+     * @param account Account to check for.
+     */
     function hasLendingPoolRole(address lendingPool, bytes32 role, address account) public view returns (bool) {
         return hasRole(_getLendingPoolRole(lendingPool, role), account);
     }
 
+    /**
+     * @notice Reverts if the system is paused.
+     */
     function requireNotPaused() external view {
         _requireNotPaused();
     }
 
     /* ========== EXTERNAL MUTATIVE FUNCTIONS ========== */
 
+    /**
+     * @notice Grants role to an account for a lending pool.
+     * @dev Can only be called by the Pool Admin or the lending pool factory.
+     * @param lendingPool Address of the lending pool.
+     * @param role Role to grant.
+     * @param account Account to grant the role to.
+     */
     function grantLendingPoolRole(address lendingPool, bytes32 role, address account)
         external
         onlyPoolAdminOrFactory(lendingPool, msg.sender)
@@ -47,6 +68,13 @@ contract KasuController is AccessControlUpgradeable, PausableUpgradeable, IKasuC
         emit LendingPoolRoleGranted(lendingPool, role, account);
     }
 
+    /**
+     * @notice Revokes role from an account for a lending pool.
+     * @dev Can only be called by the Pool Admin or the lending pool factory.
+     * @param lendingPool Address of the lending pool.
+     * @param role Role to revoke.
+     * @param account Account to revoke the role from.
+     */
     function revokeLendingPoolRole(address lendingPool, bytes32 role, address account)
         external
         onlyAdminOrPoolAdmin(lendingPool, msg.sender)
@@ -55,15 +83,28 @@ contract KasuController is AccessControlUpgradeable, PausableUpgradeable, IKasuC
         emit LendingPoolRoleRevoked(lendingPool, role, account);
     }
 
+    /**
+     * @notice Renounces a role for a lending pool.
+     * @param lendingPool Address of the lending pool.
+     * @param role Role to renounce.
+     */
     function renounceLendingPoolRole(address lendingPool, bytes32 role) external {
         renounceRole(_getLendingPoolRole(lendingPool, role), msg.sender);
         emit LendingPoolRoleRenounced(lendingPool, role, msg.sender);
     }
 
+    /**
+     * @notice Pauses the system.
+     * @dev Can only be called by the Kasu admin.
+     */
     function pause() external whenNotPaused onlyRole(ROLE_KASU_ADMIN) {
         _pause();
     }
 
+    /**
+     * @notice Unpauses the system.
+     * @dev Can only be called by the Kasu admin.
+     */
     function unpause() external whenPaused onlyRole(ROLE_KASU_ADMIN) {
         _unpause();
     }
--- src/shared/interfaces/IKasuController.sol
+++ src/shared/interfaces/IKasuController.sol
@@ -33,20 +33,6 @@ error NoReentrantRole();
 interface IKasuController is IAccessControl {
     /* ========== EXTERNAL VIEW FUNCTIONS ========== */
 
-    /**
-     * @notice Gets owner of a lending pool.
-     * @param lendingPool Lending pool.
-     * @return owner Owner of the lending pool.
-     */
-    // function lendingPoolOwner(address lendingPool) external view returns (address owner);
-
-    /**
-     * @notice Looks if an account has a role for a lending pool.
-     * @param lendingPool Address of the lending pool.
-     * @param role Role to look for.
-     * @param account Account to check.
-     * @return hasRole True if account has the role for the lending pool, false otherwise.
-     */
     function hasLendingPoolRole(address lendingPool, bytes32 role, address account)
         external
         view
@@ -56,55 +42,14 @@ interface IKasuController is IAccessControl {
 
     /* ========== EXTERNAL MUTATIVE FUNCTIONS ========== */
 
-    /**
-     * @notice Grants role to an account for a lending pool.
-     * @dev Requirements:
-     * - caller must have either role ROLE_LENDING_POOL_FACTORY or role ROLE_POOL_ADMIN for the lending pool
-     * @param lendingPool Address of the lending pool.
-     * @param role Role to grant.
-     * @param account Account to grant the role to.
-     */
     function grantLendingPoolRole(address lendingPool, bytes32 role, address account) external;
-
-    /**
-     * @notice Revokes role from an account for a lending pool.
-     * @dev Requirements:
-     * - caller must have either role ROLE_KASU_ADMIN or role ROLE_POOL_ADMIN for the lending pool
-     * @param lendingPool Address of the lending pool.
-     * @param role Role to revoke.
-     * @param account Account to revoke the role from.
-     */
     function revokeLendingPoolRole(address lendingPool, bytes32 role, address account) external;
-
     function renounceLendingPoolRole(address lendingPool, bytes32 role) external;
 
     function pause() external;
-
     function unpause() external;
 
-    /**
-     * @notice Renounce role for a lending pool.
-     * @param lendingPool Address of the lending pool.
-     * @param role Role to renounce.
-     */
-    // function renounceLendingPoolRole(address lendingPool, bytes32 role) external;
-
-    /**
-     * @notice Grant ownership to lending pool and assigns admin role.
-     * @dev Ownership can only be granted once and it should be done at vault creation time.
-     * @param lendingPool Address of the lending pool.
-     * @param owner address to which grant ownership to
-     */
-    // function grantLendingPoolOwnership(address lendingPool, address owner) external;
-
     /* ========== EVENTS ========== */
-
-    /**
-     * @notice Emitted when ownership of a lending pool is granted to an address
-     * @param lendingPool Lending pool address
-     * @param address_ Address of the new lending pool owner
-     */
-    event LendingPoolOwnershipGranted(address indexed lendingPool, address indexed address_);
 
     /**
      * @notice Lending pool specific role was granted
