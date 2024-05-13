82d7094c9d7b01ef426a3c5fe7bec6f7de5dbdb6
Nikos Chondromatidis <nchondromatidis@gmail.com>
2024-05-13 22:38:31 +0300

    minor fixes

--- scripts/runClearing.ts
+++ scripts/runClearing.ts
@@ -1,11 +1,10 @@
 import { runClearing } from './utils/runClearing';
 import { parseUnits } from 'ethers';
 
-const LENDING_POOL_ADDRESS = '0xD90cfCf44330968e5574116C41b80d699222F317';
+const LENDING_POOL_ADDRESS = '';
 const DRAW_AMOUNT = parseUnits('0', 6);
 
 async function main() {
-    // run clearing1
     await runClearing(LENDING_POOL_ADDRESS, DRAW_AMOUNT);
 }
 
